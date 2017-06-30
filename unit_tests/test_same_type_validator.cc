915e6be263ab397f6950650a9b74490114680c40
Sagi Shadur <saroad@gmail.com>
2017-07-01 02:17:18 +0300

    Added same type validator test for ratios

--- unit_tests/test_same_type_validator.cc
+++ unit_tests/test_same_type_validator.cc
@@ -7,14 +7,18 @@
 
 #include "units/include/internal/same_type_validator.h"
 #include "units/include/internal/multiply_units.h"
+#include "units/include/internal/ratio_units.h"
 #include "units/include/internal/numeric_value.h"
+#include "units/include/internal/none_type.h"
 
 #include <gtest/gtest.h>
 #include <ratio>
 
 using units::is_from_the_same_type;
 using units::Multiply;
+using units::Ratio;
 using units::NumericValue;
+using units::NoneType;
 using std::ratio;
 using namespace testing;
 
@@ -73,6 +77,9 @@ private:
 TEST_F(TestSameTypeValidator, same_types_return_true)
 {
 	check_from_the_same_type<A, A>();
+	check_from_the_same_type<Multiply<A, B>, Multiply<A, B>>();
+	check_from_the_same_type<Multiply<A, B, C>, Multiply<A, B, C>>();
+	check_from_the_same_type<Ratio<A, B>, Ratio<A, B>>();
 }
 
 TEST_F(TestSameTypeValidator, not_same_types_return_false)
@@ -80,15 +87,21 @@ TEST_F(TestSameTypeValidator, not_same_types_return_false)
 	check_not_from_the_same_type<A, B>();
 }
 
-TEST_F(TestSameTypeValidator, type_and_multiply_type_with_itself_returns_true)
+TEST_F(TestSameTypeValidator, type_and_multiply_with_only_self_returns_true)
 {
 	check_from_the_same_type<A, Multiply<A>>();
 	check_not_from_the_same_type<A, Multiply<B>>();
 }
 
+TEST_F(TestSameTypeValidator, multiply_by_self)
+{
+	check_not_from_the_same_type<Multiply<A, A>, Multiply<A>>();
+	check_not_from_the_same_type<Multiply<A, A>, Multiply<A, B>>();
+	check_not_from_the_same_type<Multiply<A, A>, Multiply<C, A>>();
+}
+
 TEST_F(TestSameTypeValidator, multiply_is_commutative_with_two_arguments)
 {
-	check_from_the_same_type<Multiply<A, B>, Multiply<A, B>>();
 	check_from_the_same_type<Multiply<A, B>, Multiply<B, A>>();
 	check_not_from_the_same_type<Multiply<A, B>, Multiply<A, C>>();
 	check_not_from_the_same_type<Multiply<A, B>, Multiply<C, A>>();
@@ -132,4 +145,32 @@ TEST_F(TestSameTypeValidator, same_type_with_multiply_inside_multiply)
 	check_not_from_the_same_type<
 		Multiply<A, B, C>,
 		Multiply<Multiply<A, B, D>>>();
+}
+
+TEST_F(TestSameTypeValidator, ratio_of_unit_and_itself_is_none)
+{
+	check_from_the_same_type<Ratio<A, A>, NoneType>();
+	check_not_from_the_same_type<Ratio<A, B>, NoneType>();
+}
+
+TEST_F(TestSameTypeValidator, multiply_by_ratio)
+{
+	check_from_the_same_type<Multiply<A, Ratio<B, C>>, Ratio<Multiply<A, B>, C>>();
+	check_from_the_same_type<Multiply<Ratio<B, C>, A>, Ratio<Multiply<A, B>, C>>();
+	check_not_from_the_same_type<Multiply<A, Ratio<B, C>>, Ratio<Multiply<A, B>, E>>();
+	check_not_from_the_same_type<Multiply<Ratio<B, C>, A>, Ratio<Multiply<A, E>, C>>();
+}
+
+TEST_F(TestSameTypeValidator, ratio_of_ratios)
+{
+	check_from_the_same_type<
+		Ratio<Ratio<A, B>, Ratio<C, D>>,
+		Ratio<Multiply<A, D>, Multiply<B, C>>>();
+}
+
+TEST_F(TestSameTypeValidator, reduced_unit)
+{
+	check_from_the_same_type<Ratio<Multiply<A, B>, B>, A>();
+	check_from_the_same_type<Ratio<Multiply<A, B, C>, Multiply<B>>, Multiply<A, C>>();
+	check_from_the_same_type<Ratio<Multiply<A, C>, Multiply<B, C>>, Ratio<A, B>>();
 }

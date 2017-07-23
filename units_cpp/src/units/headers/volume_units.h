936b25b18a2a7ad2fe11d7fa899c11eca1729d92
Sagi Shadur <saroad@gmail.com>
2017-07-23 07:49:12 +0300

    Volume units are also generated now
    related to #11

--- units_cpp/src/units/headers/volume_units.h
+++ units_cpp/src/units/headers/volume_units.h
@@ -1,49 +1,27 @@
-/*
- * volume_units.h
- *
- *  Created on: Jun 30, 2017
- *      Author: sagis
+/* This file is auto generated.
+ * DO NOT EDIT IT MANUALY!
  */
 
 #ifndef INCLUDE_VOLUME_UNITS_H_
 #define INCLUDE_VOLUME_UNITS_H_
 
-#include "internal/units_pow_type.h"
-#include "internal/multiplyer_scales.h"
+#include "internal/numeric_value.h"
 #include "internal/utils.h"
 #include "length_units.h"
 
-#include <ratio>
+#include "tags/volume_tags.h"
+
 namespace units {
 namespace volume
 {
 
-struct volume_tag
-{
-	using code = pow_type_tag<length::tags::length_tag, 3>::code;
-};
+using CubicFeet = NumericValue<tags::cubic_feet_tag, tags::volume_tag>;
+using CubicYards = NumericValue<tags::cubic_yards_tag, tags::volume_tag>;
+using CubicMeters = NumericValue<tags::cubic_meters_tag, tags::volume_tag>;
+using CubicKilometers = NumericValue<tags::cubic_kilometers_tag, tags::volume_tag>;
+using Liters = NumericValue<tags::liters_tag, tags::volume_tag>;
+using Milliliters = NumericValue<tags::milliliters_tag, tags::volume_tag>;
 
-using CubicFeet = Cubic<length::Feet>;
-using CubicYards = Cubic<length::Yards>;
-using CubicMeters = Cubic<length::Meters>;
-using CubicKilometers = Cubic<length::Kilometers>;
-
-struct liters_scale_tag
-{
-	static constexpr double scale = milli * CubicMeters::scale;
-	static std::string singularName();
-	static std::string pluralName();
-};
-
-struct milliliters_scale_tag
-{
-	static constexpr double scale = milli * liters_scale_tag::scale;
-	static std::string singularName();
-	static std::string pluralName();
-};
-
-using Liters = NumericValue<liters_scale_tag, volume_tag>;
-using Milliliters = NumericValue<milliliters_scale_tag, volume_tag>;
 
 USER_DEFINED_LITERALS(CubicFeet, cubic_feet);
 USER_DEFINED_LITERALS(CubicYards, cubic_yards);
@@ -51,6 +29,8 @@ USER_DEFINED_LITERALS(CubicMeters, cubic_meters);
 USER_DEFINED_LITERALS(CubicKilometers, cubic_kilometers);
 USER_DEFINED_LITERALS(Liters, liters);
 USER_DEFINED_LITERALS(Milliliters, milliliters);
+
+
 }
 }
 

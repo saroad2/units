88c729c65452f42b5ee01885eb5779d829733184
Sagi Shadur <saroad@gmail.com>
2017-07-23 08:09:35 +0300

    Energy units are also generated now
    related to #11

--- units_cpp/src/units/headers/energy_units.h
+++ units_cpp/src/units/headers/energy_units.h
@@ -1,57 +1,31 @@
-/*
- * energy_units.h
- *
- *  Created on: Jul 5, 2017
- *      Author: sagis
+/* This file is auto generated.
+ * DO NOT EDIT IT MANUALY!
  */
 
 #ifndef INCLUDE_ENERGY_UNITS_H_
 #define INCLUDE_ENERGY_UNITS_H_
 
 #include "internal/numeric_value.h"
-#include "internal/units_ratio_type.h"
-#include "force_units.h"
-#include "length_units.h"
 #include "internal/utils.h"
+#include "mass_units.h"
+#include "duration_units.h"
 
-#include <ratio>
+#include "tags/energy_tags.h"
 
 namespace units {
 namespace energy
 {
 
-struct energy_tag
-{
-	using code = ratio_type_tag<force::tags::force_tag, length::tags::length_tag>::code;
-};
+using Joules = NumericValue<tags::joules_tag, tags::energy_tag>;
+using Kilojoules = NumericValue<tags::kilojoules_tag, tags::energy_tag>;
+using Megajoules = NumericValue<tags::megajoules_tag, tags::energy_tag>;
 
-struct joules_tag
-{
-	static constexpr double scale = Multiply<force::Newtons, length::Meters>::scale;
-	static std::string singularName();
-	static std::string pluralName();
-};
-
-struct kilojoules_tag
-{
-	static constexpr double scale = kilo * joules_tag::scale;
-	static std::string singularName();
-	static std::string pluralName();
-};
-
-struct megajoules_tag
-{
-	static constexpr double scale = mega * joules_tag::scale;
-	static std::string singularName();
-	static std::string pluralName();
-};
-using Joules = NumericValue<joules_tag, energy_tag>;
-using Kilojoules = NumericValue<kilojoules_tag, energy_tag>;
-using Megajoules = NumericValue<megajoules_tag, energy_tag>;
 
 USER_DEFINED_LITERALS(Joules, joules);
 USER_DEFINED_LITERALS(Kilojoules, kilojoules);
 USER_DEFINED_LITERALS(Megajoules, megajoules);
+
+
 }
 }
 

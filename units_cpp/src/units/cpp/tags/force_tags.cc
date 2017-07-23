ad1a941c8453a5dac14aa8238b26639f780fdb87
Sagi Shadur <saroad@gmail.com>
2017-07-23 07:53:37 +0300

    Force units are also generated now
    fixes #10

---
+++ units_cpp/src/units/cpp/tags/force_tags.cc
@@ -0,0 +1,24 @@
+/* This file is auto generated.
+ * DO NOT EDIT IT MANUALY!
+ */
+
+#include "tags/force_tags.h"
+
+namespace units {
+namespace force {
+namespace tags
+{
+
+using std::string;
+
+string newtons_tag::singularName() { return "newton"; }
+string newtons_tag::pluralName() { return "newtons"; }
+
+string dynes_tag::singularName() { return "dyne"; }
+string dynes_tag::pluralName() { return "dynes"; }
+
+
+
+}
+}
+}
--- units_cpp/src/units/headers/force_units.h
+++ units_cpp/src/units/headers/force_units.h
@@ -1,59 +1,31 @@
-/*
- * power_units.h
- *
- *  Created on: Jul 4, 2017
- *      Author: sagis
+/* This file is auto generated.
+ * DO NOT EDIT IT MANUALY!
  */
 
 #ifndef INCLUDE_FORCE_UNITS_H_
 #define INCLUDE_FORCE_UNITS_H_
 
 #include "internal/numeric_value.h"
-#include "internal/units_pow_type.h"
-#include "internal/units_ratio_type.h"
 #include "internal/utils.h"
-#include "length_units.h"
 #include "mass_units.h"
+#include "length_units.h"
 #include "duration_units.h"
 
-#include <ratio>
-#include "internal/units_multiply_type.h"
+#include "tags/force_tags.h"
 
 namespace units {
 namespace force
 {
 
-struct force_tag
-{
-	using code = ratio_type_tag<multiply_type_tag<mass::tags::mass_tag, length::tags::length_tag>,
-								pow_type_tag<duration::tags::duration_tag, 2>>::code;
-};
+using Newtons = NumericValue<tags::newtons_tag, tags::force_tag>;
+using Dynes = NumericValue<tags::dynes_tag, tags::force_tag>;
 
-struct newtons_tag
-{
-	constexpr static double scale = Ratio<
-			Multiply<mass::Kilograms, length::Meters>,
-			Square<duration::Seconds>>::scale;
-	static std::string singularName();
-	static std::string pluralName();
-};
-
-using Newtons = NumericValue<newtons_tag, force_tag>;
-
-struct dynes_tag
-{
-	constexpr static double scale = Newtons::scale * 1e-5;
-	static std::string singularName();
-	static std::string pluralName();
-};
-
-using Dynes = NumericValue<dynes_tag, force_tag>;
 
 USER_DEFINED_LITERALS(Newtons, newtons);
 USER_DEFINED_LITERALS(Dynes, dynes);
 
-}
-}
 
+}
+}
 
 #endif /* INCLUDE_FORCE_UNITS_H_ */

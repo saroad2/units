2b9ca95748465c28730fb26d00d83926c679ecc0
Sagi Shadur <saroad@gmail.com>
2017-11-04 18:32:27 +0200

    Units_Generator - now generates planetary rotation peroids
    related to #63

--- units_cpp/src/units/headers/units/constants/planetary_rotation_periods.h
+++ units_cpp/src/units/headers/units/constants/planetary_rotation_periods.h
@@ -1,30 +1,29 @@
-/*
- * planetary_rotation_periods.h
- *
- *  Created on: Nov 3, 2017
- *      Author: oriash93
+/* This file is auto generated.
+ * DO NOT EDIT IT MANUALLY!
  */
 
-#ifndef SRC_UNITS_HEADERS_UNITS_CONSTANTS_PLANETARY_ROTATION_PERIODS_H_
-#define SRC_UNITS_HEADERS_UNITS_CONSTANTS_PLANETARY_ROTATION_PERIODS_H_
+#ifndef _PLANETARY_ROTATION_PERIODS_
+#define _PLANETARY_ROTATION_PERIODS_
 
 #include <units/duration_units.h>
+
 
 namespace units {
 namespace planetary_rotation_periods
 {
 
 constexpr duration::Seconds sunRotationPeriod{2192831.6};
-constexpr duration::Seconds mercuryRotationPeriod{5067030};
-constexpr duration::Seconds venusRotationPeriod{-20996760};
+constexpr duration::Seconds mercuryRotationPeriod{5067030.0};
+constexpr duration::Seconds venusRotationPeriod{-2.099676E7};
 constexpr duration::Seconds earthRotationPeriod{86164.091};
 constexpr duration::Seconds marsRotationPeriod{88642.663};
-constexpr duration::Seconds jupiterRotationPeriod{35730};
-constexpr duration::Seconds saturnRotationPeriod{36840};
-constexpr duration::Seconds uranusRotationPeriod{-62064};
-constexpr duration::Seconds neptuneRotationPeriod{57996};
-    
+constexpr duration::Seconds jupiterRotationPeriod{35730.0};
+constexpr duration::Seconds saturnRotationPeriod{36840.0};
+constexpr duration::Seconds uranusRotationPeriod{-62064.0};
+constexpr duration::Seconds neptuneRotationPeriod{57996.0};
+
+
 }
 }
 
-#endif /* SRC_UNITS_HEADERS_UNITS_CONSTANTS_PLANETARY_ROTATION_PERIODS_H_ */
+#endif /* _PLANETARY_ROTATION_PERIODS_ */

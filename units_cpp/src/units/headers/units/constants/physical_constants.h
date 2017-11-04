5d25ad7b742ae156da19b939e535a84cac9052a0
Sagi Shadur <saroad@gmail.com>
2017-11-04 18:34:52 +0200

    Units_Generator - now generates description as well

--- units_cpp/src/units/headers/units/constants/physical_constants.h
+++ units_cpp/src/units/headers/units/constants/physical_constants.h
@@ -19,12 +19,21 @@ namespace units {
 namespace physical_constants
 {
 
+//the speed of light in vaccum.
 constexpr speed::MetersPerSecond speedOfLight{2.99792458E8};
+
+//the quantum of action.
 constexpr Multiply<energy::Joules, duration::Seconds> planckConstant{6.62607004E-34};
+
+//planck constant divided by 2 pi.
 constexpr Multiply<energy::Joules, duration::Seconds> reducedPlanckConstant{1.0545718E-34};
+
+//a constant reguarding the force between to objects, depending on their masses and distance
 constexpr Ratio<
 	Multiply<force::Newtons, length::Meters, length::Meters>,
 	Multiply<duration::Seconds, duration::Seconds>> gravitationalConstant{6.67408E-11};
+
+//the electric charge of a single proton
 constexpr electric_charge::Coulombs elementaryCharge{1.6021766208E-19};
 
 
--- units_cpp/src/units/headers/units/constants/planetary_rotation_periods.h
+++ units_cpp/src/units/headers/units/constants/planetary_rotation_periods.h
@@ -12,14 +12,31 @@ namespace units {
 namespace planetary_rotation_periods
 {
 
+//the time it takes the sun to make a whole spin around itself
 constexpr duration::Seconds sunRotationPeriod{2192831.6};
+
+//the time it takes mercury to make a whole spin around itself
 constexpr duration::Seconds mercuryRotationPeriod{5067030.0};
+
+//the time it takes venus to make a whole spin around itself
 constexpr duration::Seconds venusRotationPeriod{-2.099676E7};
+
+//the time it takes earth to make a whole spin around itself
 constexpr duration::Seconds earthRotationPeriod{86164.091};
+
+//the time it takes mars to make a whole spin around itself
 constexpr duration::Seconds marsRotationPeriod{88642.663};
+
+//the time it takes jupiter to make a whole spin around itself
 constexpr duration::Seconds jupiterRotationPeriod{35730.0};
+
+//the time it takes saturn to make a whole spin around itself
 constexpr duration::Seconds saturnRotationPeriod{36840.0};
+
+//the time it takes uranus to make a whole spin around itself
 constexpr duration::Seconds uranusRotationPeriod{-62064.0};
+
+//the time it takes neptune to make a whole spin around itself
 constexpr duration::Seconds neptuneRotationPeriod{57996.0};
 
 

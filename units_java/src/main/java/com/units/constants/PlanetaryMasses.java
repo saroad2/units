abfd07d86e6ca13f191a6fd2afbcdebc8fac3dae
Sagi Shadur <saroad@gmail.com>
2017-12-24 11:39:48 +0200

    Add planetary masses constants in java

---
+++ units_java/src/main/java/com/units/constants/PlanetaryMasses.java
@@ -0,0 +1,34 @@
+package com.units.constants;
+
+import com.units.mass.SolarMasses;
+
+public class PlanetaryMasses {
+	
+	//the mass of the sun
+	static public final SolarMasses sunMass = new SolarMasses(1.0);
+	
+	//the mass of mercury
+	static public final SolarMasses mercuryMass = new SolarMasses(1.6601E-7);
+	
+	//the mass of venus
+	static public final SolarMasses venusMass = new SolarMasses(2.4478383E-6);
+	
+	//the mass of earth
+	static public final SolarMasses earthMass = new SolarMasses(3.00348959632E-6);
+	
+	//the mass of mars
+	static public final SolarMasses marsMass = new SolarMasses(3.227151E-7);
+	
+	//the mass of jupiter
+	static public final SolarMasses jupiterMass = new SolarMasses(9.5479194E-4);
+	
+	//the mass of saturn
+	static public final SolarMasses saturnMass = new SolarMasses(2.85886E-4);
+	
+	//the mass of uranus
+	static public final SolarMasses uranusMass = new SolarMasses(4.366244E-5);
+
+	//the mass of neptune
+	static public final SolarMasses neptuneMass = new SolarMasses(5.151389E-5);
+
+}
